e0290e7c1972eaeb145e3c88cd9a5fa55235c9e4
pipelinerobot <pipelinerobot@nautes.io>
2024-03-15 17:22:14 +0800

    update

--- src/main/java/com/example/Controller/HelloController.java
+++ src/main/java/com/example/Controller/HelloController.java
@@ -9,6 +9,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class HelloController {
     @GetMapping
     public String sayHello() {
-        return "Hello SpringBoot v1.0.2"; 
+        return "Hello SpringBoot v1.0.3"; 
     }    
 }
